b8f1b605b4c7107c9755f5846ef254407eacf58c
tierradenadie073 <118555752+tierradenadie073@users.noreply.github.com>
2023-07-14 16:18:50 +0200

    initial commit

--- src/Vista/VistaCiudades.java
+++ src/Vista/VistaCiudades.java
@@ -16,6 +16,7 @@ import javax.swing.JFormattedTextField;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 public class VistaCiudades extends JFrame {
 
@@ -49,7 +50,7 @@ public class VistaCiudades extends JFrame {
 	 */
 	public VistaCiudades() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(200, 200, 600, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -76,29 +77,29 @@ public class VistaCiudades extends JFrame {
 			}
 		});
 		Aceptar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 12));
-		Aceptar.setBounds(335, 93, 89, 79);
+		Aceptar.setBounds(410, 71, 89, 90);
 		contentPane.add(Aceptar);
 
 		codigoPostal = new JTextField();
 		codigoPostal.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 18));
-		codigoPostal.setBounds(127, 87, 180, 90);
+		codigoPostal.setBounds(179, 71, 180, 90);
 		contentPane.add(codigoPostal);
 		codigoPostal.setColumns(10);
 
 		ciudad = new JTextField();
 		ciudad.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 18));
-		ciudad.setBounds(20, 93, 86, 79);
+		ciudad.setBounds(30, 71, 86, 90);
 		contentPane.add(ciudad);
 		ciudad.setColumns(10);
 
-		Ciudad = new JLabel("   Ciudad");
-		Ciudad.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 12));
-		Ciudad.setBounds(20, 30, 75, 30);
+		Ciudad = new JLabel(" Ciudad");
+		Ciudad.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 21));
+		Ciudad.setBounds(30, 30, 86, 30);
 		contentPane.add(Ciudad);
 
-		lblCodigoPostal = new JLabel("Codigo Postal");
-		lblCodigoPostal.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 12));
-		lblCodigoPostal.setBounds(169, 30, 75, 30);
+		lblCodigoPostal = new JLabel("  Codigo Postal");
+		lblCodigoPostal.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
+		lblCodigoPostal.setBounds(179, 30, 180, 30);
 		contentPane.add(lblCodigoPostal);
 
 		JButton listar = new JButton("listar");
@@ -107,8 +108,9 @@ public class VistaCiudades extends JFrame {
 				JOptionPane.showMessageDialog(null, controlador.listarBaseDeDatos());
 			}
 		});
-		listar.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		listar.setBounds(335, 183, 89, 67);
+		listar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 18));
+		listar.setBounds(410, 184, 89, 80);
+		
 		contentPane.add(listar);
 
 		btnEliminar = new JButton("Eliminar");
@@ -124,8 +126,8 @@ public class VistaCiudades extends JFrame {
 
 			}
 		});
-		btnEliminar.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnEliminar.setBounds(198, 183, 89, 67);
+		btnEliminar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		btnEliminar.setBounds(410, 293, 89, 74);
 		contentPane.add(btnEliminar);
 	}
 }
